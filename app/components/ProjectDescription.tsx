e7f1b980a7521bdf8a87b0a059933bda35d87fe5
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-02-24 05:34:39 -0800

    build working.

--- app/components/ProjectDescription.tsx
+++ app/components/ProjectDescription.tsx
@@ -1,7 +1,12 @@
-export default function ProjectDescription() {
+interface descriptionProps {
+    description: string;
+}
+const ProjectDescription: React.FC<descriptionProps> = ({ description }: descriptionProps) => {
     return (
         <p className="m-12 p-6 bg-gray-900 opacity-50 text-white rounded-2xl shadow-md">
-            Velma is a full-Stack Vanity URL Tool I built at LinkedIn using the Remix framework. It was built with a custom Node.JS client that I to interface with Espresso (LinkedIn's NoSQL data store), TypeScript, and React. The outcome? An 80% reduction in process time for our shareholders, and a major boost in productivity for any team who wished to use Node to interface with Espresso.
+            {description}
         </p>
     )
 }
+
+export default ProjectDescription
