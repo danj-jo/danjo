f56974864365dcb6257c6c2703069607b63cb50e
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-02-24 20:38:26 -0800

    tailwind-changes

--- app/projects/project.tsx
+++ app/projects/project.tsx
@@ -7,7 +7,7 @@ const Project = ({ title, stack }: projectProps) => {
         <div className="">
             <a href={`projects/${title.toLowerCase()}`}>
                 <div className="
-               sm:m-2 md:m-2 lg:m2 md:flex md:justify-between bg-gray-900 rounded-2xl p-16 sm:p-6 h-8 opacity-25 text-white sm:items-center hover:bg-[#9e9e9e] hover:opacity-100 transition-colors duration-300 ease-in  ">
+               overflow-y-hidden m-3 md:m-2 flex justify-around md:justify-between bg-gray-900 rounded-2xl p-8 md:p-6 h-8 opacity-25 text-white items-center hover:bg-[#9e9e9e] hover:opacity-100 transition-colors duration-300 ease-in  ">
                     <p> {title} </p>
                     <p> {stack.toUpperCase()} </p>
                 </div>
