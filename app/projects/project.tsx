e68fa51152288deaaca9c069b655931ada11a6bb
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-02-24 19:28:25 -0800

    responsive-changes

--- app/projects/project.tsx
+++ app/projects/project.tsx
@@ -6,7 +6,8 @@ const Project = ({ title, stack }: projectProps) => {
     return (
         <div className="">
             <a href={`projects/${title.toLowerCase()}`}>
-                <div className="m-2 flex justify-between bg-gray-900 rounded-2xl p-6 h-8 opacity-25 text-white items-center hover:bg-[#9e9e9e] hover:opacity-100 transition-colors duration-300 ease-in ">
+                <div className="
+               sm:m-2 md:m-2 lg:m2 md:flex md:justify-between bg-gray-900 rounded-2xl p-16 sm:p-6 h-8 opacity-25 text-white sm:items-center hover:bg-[#9e9e9e] hover:opacity-100 transition-colors duration-300 ease-in  ">
                     <p> {title} </p>
                     <p> {stack.toUpperCase()} </p>
                 </div>
